5ab09a71401771b0875001d57026c88d71428903
MasonWillisA <masonwillis@ou.edu>
2026-08-31 11:55:59 -0500

    Implement membership discount feature
    Added membership discount logic based on user input.

--- HotelExample/Program.cs
+++ HotelExample/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+double pre_discount_price = 100.0;
+double discount = 0.0;
+
+Console.WriteLine("\nPlease enter your membership type: \nGold, \nSilver, or \nGuest");
+string mType = Console.ReadLine();
+
+if (mType == "Gold")
+{
+    Console.WriteLine("Welcome, Gold Member!");
+    discount = 0.2;
+}
+else if (mType == "Silver")
+{
+    Console.WriteLine("Welcome, Silver Member!");
+    discount = 0.1;
+}
+else
+{
+    Console.WriteLine("Welcome, Guest!");
+    discount = 0.0;
+}
